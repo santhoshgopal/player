9df35bcffb72bfe1a80aa1fe11978543b673bac9
santhoshgopal <santhoshgopal32@gmail.com>
2016-08-23 11:56:49 +0530

    Update fact.java

--- fact.java
+++ fact.java
@@ -6,8 +6,6 @@ public static void main(String args[])
 int sum=1,i;
 Scanner in=new Scanner(System.in);
 int n=in.nextInt();
-if(n==1)
-System.out.println("1");
 for(i=1;i<=n;i++)
 sum=sum*i;
 System.out.println(sum);
